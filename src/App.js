b0fc7af395cd7cdf5e034d34023423833a0697f4
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-10-13 01:26:50 +0600

    Home css

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       children:[
         {
           path: '/',
+          loader: () =>fetch('tshirt.json'),
           element: <Home></Home>
         },
         {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import './Home.css'
 
 const Home = () => {
+    const tshirts = useLoaderData();
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='home-container'>
+            <div className="t-shirt-container">
+            {
+                tshirts.map(tshirt=> <TShirt key={tshirt.id} tshirt = {tshirt}></TShirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
