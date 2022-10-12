812ba3a07abab4acdcd9fa370ff6cbf252ea4318
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-10-13 01:49:20 +0600

    Cart product show'

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart}) => {
     return (
         <div>
             <h2>Order Summary</h2>
+            <h5>Order quantity: {cart.length}</h5>
+            {
+                cart.map(tshirt => <p key={tshirt._id}>
+                    {tshirt.name}
+                    <button>X</button>
+                </p>)
+            }
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
@@ -6,15 +6,27 @@ import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = tshirt =>{
+        const exists = cart.find(ts => ts._id===tshirt._id);
+        if(exists){
+            alert('t-shirt already added');
+        }else{
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            alert('successfully added')
+        }
+        
+    }
     return (
         <div className='home-container'>
             <div className="t-shirt-container">
             {
-                tshirts.map(tshirt=> <TShirt key={tshirt.id} tshirt = {tshirt}></TShirt>)
+                tshirts.map(tshirt=> <TShirt key={tshirt._id} tshirt = {tshirt} handleAddToCart={handleAddToCart}></TShirt>)
             }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart cart = {cart}></Cart>
             </div>
         </div>
     );
