be9d341dd7f8970a50f3a9a2621e1beeaff38c0f
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-10-13 22:34:08 +0600

    product remove button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Cart = ({cart}) => {
+const Cart = ({cart, handleRemoveItem}) => {
     return (
         <div>
             <h2>Order Summary</h2>
@@ -8,7 +8,7 @@ const Cart = ({cart}) => {
             {
                 cart.map(tshirt => <p key={tshirt._id}>
                     {tshirt.name}
-                    <button>X</button>
+                    <button onClick={()=>handleRemoveItem(tshirt)}>X</button>
                 </p>)
             }
         </div>
